5c6195427d7396b29812316f0b3e8820dcf1ecd2
Nazar.M <inazar.m@gmail.com>
2020-09-22 17:13:06 +0500

    get data from database ansshow in table.

---
+++ DynamicallyLoadingContentWithJquery Ajax_ASPNETMVC/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DynamicallyLoadingContentWithJquery_Ajax_ASPNETMVC.Models;
+
+namespace DynamicallyLoadingContentWithJquery_Ajax_ASPNETMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        employeeDBEntities db = new employeeDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.EmployeeInfoes.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DynamicallyLoadingContentWithJquery Ajax_ASPNETMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DynamicallyLoadingContentWithJquery_Ajax_ASPNETMVC.MvcApplication" Language="C#" %>
